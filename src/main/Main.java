38fd20395d8c75e8e438c639a76b9addabe2c24c
huouvcti <huouvcti@gamil.com>
2025-04-29 21:38:56 +0900

    refactor: 코드 공백 정리

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,7 +16,7 @@ public class Main {
         // Menu 객체 생성을 통해 이름 설정
         // Menu 클래스 내 있는 List<MenuItem> 에 MenuItem 객체 생성하면서 삽입
 
-        Menu burgersMenu  = new Menu("BURGERS",
+        Menu burgersMenu = new Menu("BURGERS",
                 new MenuItem("ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
                 new MenuItem("SmokeShack", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"),
                 new MenuItem("Cheeseburger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"),
@@ -30,7 +30,7 @@ public class Main {
                 new MenuItem("Water", 500, "물")
         );
 
-        Menu dessertMenu  = new Menu("DESSERTS",
+        Menu dessertMenu = new Menu("DESSERTS",
                 new MenuItem("Matcha Cookie", 3000, "말차 쿠키"),
                 new MenuItem("Chocolate Cookie", 2500, "초코 쿠키")
         );
